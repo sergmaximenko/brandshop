357931de8fc9fce15d652d36636394a0f451a745
sergmaximenko <sukavigimalka@mail.ru>
2022-04-11 17:20:44 +0300

    pagination and sort

---
+++ js/pagination.js
@@ -0,0 +1,167 @@
+let notesOnPage = 9;
+let sort = 'normal';
+
+function pagi(){
+let pagination = document.querySelector('.pagination');
+let content = document.querySelector('.content');
+pagination.innerHTML = '';
+
+
+let countOfItems = Math.ceil(adidas.length / notesOnPage);
+
+for(let i=1; i<=countOfItems; i++){
+	let li = document.createElement('li');
+	li.textContent = i;
+	pagination.appendChild(li);
+}
+
+let tabs = document.querySelectorAll('.pagination li');
+showPage(tabs[0]);
+
+for(let i =0; i<tabs.length; i++){
+	tabs[i].onclick=function(){
+		showPage(tabs[i]);
+	}
+}
+
+if(countOfItems > 5){
+	let left = document.createElement('i');
+	left.className='fa fa-angle-left faPag display0';
+	left.ariaHidden=true;
+	pagination.insertBefore(left, pagination.firstChild);
+	let right = document.createElement('i');
+	right.className='fa fa-angle-right faPag';
+	right.ariaHidden=true;
+	pagination.appendChild(right);
+	
+	let fa = document.querySelectorAll('.faPag');
+	
+	fa[1].onclick = function(){
+	let active = document.querySelector('.pagination li.active');
+	showPage(tabs[+active.innerHTML]);
+	}
+	fa[0].onclick = function(){
+	let active = document.querySelector('.pagination li.active');
+	showPage(tabs[+active.innerHTML - 2]);
+}
+}
+
+
+
+
+
+
+	function showPage(item){
+		let active = document.querySelector('.pagination li.active');
+		if(active){
+			active.classList.remove('active');
+		}
+		item.classList.add('active');
+		let pageNum = +item.innerHTML;
+		
+		if(countOfItems > 5){
+			let fa = document.querySelectorAll('.faPag');
+			if(tabs[tabs.length-1].className == 'active'){
+			fa[1].classList.add('display0');
+			} else if(tabs[tabs.length-1].className !== 'active' && fa[1]){
+				fa[1].classList.remove('display0');
+			}
+			if(tabs[0].className == 'active' && fa[0]){
+				fa[0].classList.add('display0');	
+			} else if (tabs[0].className !== 'active' && fa[0]){
+				fa[0].classList.remove('display0');
+			}
+		}
+		
+		
+		if(pageNum>=5){
+			
+		for(let i =0; i<tabs.length; i++){
+			tabs[i].classList.add('display0');
+		}
+		if(pageNum==1){
+			tabs[pageNum-1].classList.remove('display0');
+			tabs[pageNum].classList.remove('display0');
+			tabs[pageNum+1].classList.remove('display0');
+			tabs[pageNum+2].classList.remove('display0');
+			tabs[pageNum+3].classList.remove('display0');
+		} else if(pageNum==2){
+			tabs[pageNum-2].classList.remove('display0');
+			tabs[pageNum-1].classList.remove('display0');
+			tabs[pageNum].classList.remove('display0');
+			tabs[pageNum+1].classList.remove('display0');
+			tabs[pageNum+2].classList.remove('display0');
+		} else if(pageNum>tabs.length - 1){
+			tabs[pageNum-5].classList.remove('display0');
+			tabs[pageNum-4].classList.remove('display0');
+			tabs[pageNum-3].classList.remove('display0');
+			tabs[pageNum-2].classList.remove('display0');
+			tabs[pageNum-1].classList.remove('display0');
+		}else if(pageNum>tabs.length - 2){
+			tabs[pageNum-4].classList.remove('display0');
+			tabs[pageNum-3].classList.remove('display0');
+			tabs[pageNum-2].classList.remove('display0');
+			tabs[pageNum-1].classList.remove('display0');
+			tabs[pageNum].classList.remove('display0');
+		}  else if(pageNum>2){
+			tabs[pageNum-3].classList.remove('display0');
+			tabs[pageNum-2].classList.remove('display0');
+			tabs[pageNum-1].classList.remove('display0');
+			tabs[pageNum].classList.remove('display0');
+			tabs[pageNum+1].classList.remove('display0');
+		}
+		}
+		
+		
+		let start = (pageNum - 1) * notesOnPage;
+		let end = start + notesOnPage;
+		let massive;
+		if(sort == 'normal'){
+			massive = adidas.slice().reverse().slice(start, end);
+		} else if(sort == '+'){
+			massive = adidas.slice().sort(min).slice(start, end);
+		} else if(sort == '-'){
+			massive = adidas.slice().sort(min).reverse().slice(start, end);
+		} else if(sort == 'new'){
+			massive = adidas.slice().reverse().slice(start, end);
+		} else if(sort == 'old'){
+			massive = adidas.slice(start, end);
+		}
+		let notes = massive;
+		content.innerHTML = '';
+		for(let i=0; i<notes.length; i++){
+			let item = document.createElement('div');
+			item.className = 'item';
+			content.appendChild(item);
+			
+			let shadow = document.createElement('div');
+			shadow.className = 'shadow';
+			item.appendChild(shadow);
+			
+			let img = document.createElement('img');
+			img.src = notes[i].img;
+			item.appendChild(img);
+			
+			let h1 = document.createElement('h1');
+			h1.textContent = notes[i].brand;
+			item.appendChild(h1);
+			
+			let p = document.createElement('p');
+			item.appendChild(p);
+			
+			let type = document.createElement('span');
+			type.textContent = notes[i].type;
+			p.appendChild(type);
+			let model = document.createElement('span');
+			model.textContent = notes[i].model;
+			p.appendChild(model);
+			
+			let h3 = document.createElement('h3');
+			h3.textContent = notes[i].price;
+			item.appendChild(h3);
+		}
+ 	
+	}	
+}
+pagi();
+
---
+++ js/sort.js
@@ -0,0 +1,73 @@
+let butSortOne = document.querySelectorAll('.sortOne div');
+let butSortTwo = document.querySelectorAll('.sortTwo div');
+let h2One = document.querySelectorAll('.sortOne div h2');
+let h2Two = document.querySelectorAll('.sortTwo div h2');
+let spanSortOne = document.querySelectorAll('.sortOne div h1 span');
+let spanSortTwo = document.querySelectorAll('.sortOne div h2 span');
+
+butSortOne[0].onclick = function(){
+	butSortOne[1].classList.toggle('show');
+}
+butSortTwo[0].onclick = function(){
+	butSortTwo[1].classList.toggle('show');
+}
+
+
+for(let i=0; i<h2One.length; i++){
+	h2One[i].onclick = function(){
+		for(let i=0; i<h2One.length; i++){
+		h2One[i].classList.remove('active');
+		h2One[i].span = spanSortTwo[i].textContent;
+		}
+		h2One[i].classList.add('active');
+		butSortOne[1].classList.toggle('show');
+		spanSortOne[0].textContent = h2One[i].span;
+		notesOnPage = +h2One[i].span
+		pagi();
+	}
+}
+
+for(let i=0; i<h2Two.length; i++){
+	h2Two[i].onclick = function(){
+		for(let i=0; i<h2Two.length; i++){
+		h2Two[i].classList.remove('active');
+		}
+		h2Two[i].classList.add('active');
+		butSortTwo[1].classList.toggle('show');
+		if(i == 0){
+			sort = 'normal';
+			pagi();
+		}
+		else if(i == 1){
+			sort = '+';
+			pagi();
+		} else if(i == 2){
+			sort = '-';
+			pagi();
+		} else if(i == 3){
+			sort = 'new';
+			pagi();
+		} else if(i == 4){
+			sort = 'old';
+			pagi();
+		}
+	}
+}
+
+//По убыванию
+
+function toNumber(a){
+	let price = a.price.split(' ');
+	price.pop();
+	price = +price.join('');
+	return price;
+}
+
+function min(a, b){
+		let aPrice = toNumber(a);
+		let bPrice = toNumber(b);
+	 	if (aPrice > bPrice) return 1;
+  		if (aPrice == bPrice) return 0;
+  		if (aPrice < bPrice) return -1;
+}
+
